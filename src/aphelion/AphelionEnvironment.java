0b4c61fb2053e7ae2cd4c691602725e0ced4ace5
Benjamin Wong <benjamin.wong@brentwood.ca>
2015-04-23 13:07:46 -0700

    V1.2 Alpha Includes new algorithm to generate crude continent shapes

--- src/aphelion/AphelionEnvironment.java
+++ src/aphelion/AphelionEnvironment.java
@@ -8,34 +8,38 @@ package aphelion;
 import environment.Environment;
 import grid.Grid;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import tilesInfrastructure.Overlay;
+import tilesInfrastructure.TerrainTypeIntf;
+import tilesInfrastructure.Texture;
+import tilesInfrastructure.TileMap;
+import tilesInfrastructure.TileMapVisualizer;
+import tilesInfrastructure.TileProviderIntf;
 
 /**
  *
  * @author Benjamin
  */
-class AphelionEnvironment extends Environment implements MapDrawDataIntf {
-    
+class AphelionEnvironment extends Environment implements MapDrawDataIntf, TileProviderIntf, TerrainTypeIntf {
+
     //<editor-fold defaultstate="collapsed" desc="Agenda">
     /**
-     * V1.0 Alpha (27/03/15) (Finish the following by 03/04/15)
-     * - implement the following:
-     *      - *Done* (Without nice icon) Fuel (A nice icon would be cool)
-     *      - Starting Asteroid with complimentary map (Need graphics designer)
-     *      - Map Portals (Enter a new map when you step on a planet in the system Map)
-     * - change:
-     *      - *Done* visiblePoints from ArrayList to mapPoints[][] 2D Array that stores visibility 
-     * - suggestions?:
-     *      - enter here
+     * V1.0 Alpha (27/03/15) (Finish the following by 03/04/15) - implement the
+     * following: - *Done* (Without nice icon) Fuel (A nice icon would be cool)
+     * - Starting Asteroid with complimentary map (Need graphics designer) - Map
+     * Portals (Enter a new map when you step on a planet in the system Map) -
+     * change: - *Done* visiblePoints from ArrayList to mapPoints[][] 2D Array
+     * that stores visibility - suggestions?: - enter here
      */
 //</editor-fold>
-    
     //<editor-fold defaultstate="collapsed" desc="AphelionEnvironment">
     public AphelionEnvironment() {
         for (int i = 0; i < grid.getRows(); i++) {
@@ -54,24 +58,21 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
         grid = new Grid(101, 101, 15, 15, new Point(25, 25), Color.BLACK);
         human_bean = new Character();
         human_bean.setMapDrawData(this);
-        mapPoints = new ArrayList<>();
-        
-        setMapPointsBeta(new int[grid.getColumns()][grid.getRows()]);
-        
-        setObjects(new ArrayList<>());
-        
-        for (int i = 0; i < grid.getColumns(); i++) {
-            for (int j = 0; j < grid.getRows(); j++) {
-                getMapPointsBeta()[i][j] = 0;
-            }
-        }
+
+        texture = new Texture();
+        overlay = new Overlay();
+
+        tileMap = new TileMap(null, new Dimension(16, 16), new Dimension(110, 60));
+        tileMap.setMapVisualizer(new TileMapVisualizer(this));
+
+        tileMap.setMap(randomContinents());
     }
 //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="timerTaskHandler">
     @Override
     public void timerTaskHandler() {
-        
+
     }
 //</editor-fold>
 
@@ -112,7 +113,7 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
     //<editor-fold defaultstate="collapsed" desc="environmentMouseClicked">
     @Override
     public void environmentMouseClicked(MouseEvent e) {
-        
+
     }
 //</editor-fold>
 
@@ -128,21 +129,27 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
                 RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 //</editor-fold>
-        for (Point mapPoint : mapPoints) {
-            Point topLeft = grid.getCellSystemCoordinate(mapPoint);
-            if (visiblePoints[mapPoint.getLocation().x][mapPoint.getLocation().y] == 1) {
-                graphics.setColor(Color.BLACK);
-            } else {
-                graphics.setColor(Color.GRAY);
-            }
-            graphics.fillRect(topLeft.x, topLeft.y, grid.getCellWidth(), grid.getCellHeight());
+//        for (Point mapPoint : mapPoints) {
+//            Point topLeft = grid.getCellSystemCoordinate(mapPoint);
+//            if (visiblePoints[mapPoint.getLocation().x][mapPoint.getLocation().y] == 1) {
+//                graphics.setColor(Color.BLACK);
+//            } else {
+//                graphics.setColor(Color.GRAY);
+//            }
+//            graphics.fillRect(topLeft.x, topLeft.y, grid.getCellWidth(), grid.getCellHeight());
+//        }
+//        for (SpaceObject object : getObjects()) {
+//            if (visiblePoints[object.getLocation().x][object.getLocation().y] == 1) {
+//                object.paintObject(graphics);
+//            }
+//        }
+        if (tileMap != null) {
+            tileMap.drawMap(graphics);
         }
-        for (SpaceObject object : getObjects()) {
-            if (visiblePoints[object.getLocation().x][object.getLocation().y] == 1) {
-                object.paintObject(graphics);
-            }
-        }
-        if (human_bean != null/** && human_bean.getScannedLocations() != null*/) {
+        if (human_bean != null/**
+                 * && human_bean.getScannedLocations() != null
+                 */
+                ) {
             human_bean.paint(graphics);
 //            human_bean.drawScanned(graphics);
         }
@@ -156,9 +163,13 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
     private ArrayList<SpaceObject> objects = new ArrayList<>();
     private int[][] visiblePoints = new int[grid.getColumns()][grid.getRows()];
     private Character human_bean;
-    
-//</editor-fold>
 
+    private TileMap tileMap;
+    private Texture texture;
+    private Overlay overlay;
+
+//</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Interfaces">
     //<editor-fold defaultstate="collapsed" desc="MapDrawDataIntf">
     /**
      * @return the gridLocations
@@ -193,7 +204,27 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
         return grid.getRows();
     }
 //</editor-fold>
-    
+
+    //<editor-fold defaultstate="collapsed" desc="TileProviderIntf">
+    @Override
+    public Image getTileTexture(Integer iD) {
+        return texture.getTexture(iD);
+    }
+
+    @Override
+    public Image getTileOverlay(Integer iD) {
+        return overlay.getOverlay(iD);
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="TerrainTypeIntf">
+    @Override
+    public String getTerrainType(Integer iD) {
+        return texture.getTerrainType(iD);
+    }
+//</editor-fold>
+//</editor-fold>
+
     //<editor-fold defaultstate="collapsed" desc="Setters/Getters">
     /**
      * @return the gridPoints
@@ -201,35 +232,35 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
     public ArrayList<SpaceObject> getGridPoints() {
         return getObjects();
     }
-    
+
     /**
      * @param gridLocations the gridLocations to set
      */
     public void setGridLocations(ArrayList<Point> gridLocations) {
         this.mapPoints = gridLocations;
     }
-    
+
     /**
      * @param gridPoints the gridPoints to set
      */
     public void setGridPoints(ArrayList<SpaceObject> gridPoints) {
         this.setObjects(gridPoints);
     }
-    
+
     /**
      * @return the objects
      */
     public ArrayList<SpaceObject> getObjects() {
         return objects;
     }
-    
+
     /**
      * @param objects the objects to set
      */
     public void setObjects(ArrayList<SpaceObject> objects) {
         this.objects = objects;
     }
-    
+
     /**
      * @return the mapPointsBeta
      */
@@ -244,7 +275,7 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
         this.visiblePoints = mapPointsBeta;
     }
 //</editor-fold>
-    
+
     //<editor-fold defaultstate="collapsed" desc="Other Methods">
     public void updateScannedArea() {
         for (Point revealedLocation : human_bean.getScannedLocations()) {
@@ -253,11 +284,100 @@ class AphelionEnvironment extends Environment implements MapDrawDataIntf {
             }
         }
     }
-    
+
     private void move(KeyEvent e) {
         human_bean.move(e);
         updateScannedArea();
     }
-//</editor-fold>
 
+    private static int[][] getRandomArray() {
+        int[][] array = new int[120][60];
+        for (int col = 0; col < array.length; col++) {
+            for (int row = 0; row < array[col].length; row++) {
+                array[col][row] = 601;
+            }
+        }
+        ArrayList<Point> sparks = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            int x = (int) (Math.random() * (array.length));
+            int y = (int) (Math.random() * (array[x].length));
+            sparks.add(new Point(x, y));
+        }
+        for (int k = 0; k < sparks.size(); k++) {
+
+            int radius = (int) ((Math.random() * 6) + 2);
+            for (int i = -radius; i <= radius; i++) {
+                for (int j = -(radius - Math.abs(i)); j <= radius - Math.abs(i); j++) {
+                    int newX = sparks.get(k).x + j;
+                    int newY = sparks.get(k).y + i;
+
+                    if ((newX >= 0) && (newX <= array.length - 1) && (newY >= 0) && (newY <= array[0].length - 1)) {
+                        array[newX][newY] = 400;
+                        if (Math.random() > 0.5) {
+                            array[newX][newY] += 100;
+                        }
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[][] randomContinents() {
+        int[][] array = new int[110][60];
+        // Background terrain type
+        for (int col = 0; col < array.length; col++) {
+            for (int row = 0; row < array[col].length; row++) {
+                array[col][row] = 500;
+            }
+        }
+        ArrayList<Point> sparks = new ArrayList<>();
+        int continents = (int) (Math.random() * 4) + 2;
+        for (int i = 0; i < continents; i++) {
+            int x = (int) (Math.random() * (array.length));
+            int y = (int) (Math.random() * (array[x].length));
+            sparks.add(new Point(x, y));
+        }
+
+        for (int k = 0; k < sparks.size(); k++) {
+            ArrayList<Point> bumps = new ArrayList<>();
+            int radius = (int) ((Math.random() * 9) + 8);
+            for (int i = -radius; i <= radius; i++) {
+                for (int j = -(radius - Math.abs(i)); j <= radius - Math.abs(i); j++) {
+                    int newX = sparks.get(k).x + j;
+                    int newY = sparks.get(k).y + i;
+                    if ((newX >= 0) && (newX <= array.length - 1) && (newY >= 0) && (newY <= array[0].length - 1)) {
+                        array[newX][newY] = 401;
+                        if (Math.abs(newX - sparks.get(k).x) + Math.abs(newY - sparks.get(k).y) == radius) {
+//                            array[newX][newY] = 401;
+                            if (Math.random() > .76) {
+                                bumps.add(new Point(newX, newY));
+                            }
+                        }
+                    }
+                }
+            }
+            int randomBump = (int) (Math.random() * bumps.size() -1);
+            for (int l = 0; l < bumps.size(); l++) {
+                int bumpRadius = 0;
+                if (bumps.indexOf(l) == randomBump) {
+                    bumpRadius = (int) ((Math.random() * 5) + 5);
+                } else {
+                    bumpRadius = (int) ((Math.random() * 4) + 1);
+                }
+                for (int i = -bumpRadius; i <= bumpRadius; i++) {
+                    for (int j = -(bumpRadius - Math.abs(i)); j <= bumpRadius - Math.abs(i); j++) {
+                        int newX = bumps.get(l).x + j;
+                        int newY = bumps.get(l).y + i;
+                        if ((newX >= 0) && (newX <= array.length - 1) && (newY >= 0) && (newY <= array[0].length - 1)) {
+                            array[newX][newY] = 401;
+                        }
+                    }
+                }
+            }
+        }
+
+        return array;
+    }
+//</editor-fold>
 }
---
+++ src/tilesInfrastructure/TileMapVisualizer.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tilesInfrastructure;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import map.Map;
+import map.MapVisualizerIntf;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class TileMapVisualizer implements MapVisualizerIntf{
+
+    private TileProviderIntf tileProvider;
+    private final Color FOG_OF_WAR = new Color(255, 255, 255, 150);
+    
+    public TileMapVisualizer(TileProviderIntf tileProvider){
+        this.tileProvider = tileProvider;
+    }
+    
+    @Override
+    public void draw(Map map, Graphics graphics) {
+//        System.out.println("Drawing map...");
+        int[][] mapData = ((TileMap) map).getMap();
+        for (int column = 0; column < mapData.length; column++){
+            for (int row = 0; row < mapData[column].length; row++) {
+//                graphics.drawString(String.valueOf(mapData[column][row]), map.getPosition().x + (row * map.getCellWidth()), map.getPosition().y + ((column + 1) * map.getCellHeight()));
+                int cellData = mapData[column][row];
+                
+                Point topLeft = new Point(column, row);
+                graphics.drawImage(getTexture(cellData), map.getCellSystemCoordinate(topLeft).x, map.getCellSystemCoordinate(topLeft).y, null);
+                graphics.drawImage(getOverlay(cellData), map.getCellSystemCoordinate(topLeft).x, map.getCellSystemCoordinate(topLeft).y, null);
+                
+                graphics.setColor(FOG_OF_WAR);
+                if (cellData % 10 == 0){
+                    graphics.fillRect(map.getCellSystemCoordinate(topLeft).x, map.getCellSystemCoordinate(topLeft).y, map.getCellWidth(), map.getCellWidth());
+                } 
+            
+            }
+        }
+    }
+    
+    private Image getTexture(int data){
+        return tileProvider.getTileTexture((int) data / 100);
+    }
+    
+    private Image getOverlay(int data){
+        return tileProvider.getTileOverlay((int) (data / 10) % 10);
+    }
+
+    /**
+     * @return the tileProvider
+     */
+    public TileProviderIntf getTileProvider() {
+        return tileProvider;
+    }
+
+    /**
+     * @param tileProvider the tileProvider to set
+     */
+    public void setTileProvider(TileProviderIntf tileProvider) {
+        this.tileProvider = tileProvider;
+    }
+}
